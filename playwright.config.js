2cc308c59cdf60fe1fed8440322ea4950505cc85
jessiejiechen0206 <jessie.chen@keypathedu.com.au>
2025-11-19 16:17:38 +1100

    Create playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  timeout: 30000,
+  retries: 1,
+  use: {
+    headless: true,
+    screenshot: "only-on-failure",
+    trace: "retain-on-failure"
+  },
+});
